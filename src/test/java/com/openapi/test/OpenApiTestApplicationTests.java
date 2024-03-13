e79a23bfd667bf328e87fde597eb633185547131
Chandra <ch-sadan.chandra@capgemini.com>
2024-03-13 12:23:42 +0530

    first commit

---
+++ src/test/java/com/openapi/test/OpenApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
